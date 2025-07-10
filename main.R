f8e9e809af6590c6d95b9c6b67e0d3a244a44611
Kyle <kylefossen21@gmail.com>
2025-07-09 19:11:29 -0700

    Added large dataset, messed around with vectors

--- main.R
+++ main.R
@@ -1,5 +1,21 @@
-nums2011 <- read.csv("nums2011.csv")
-prec2011 <- mean(sort(nums2011$PRCP))
-print(prec2011)
+avgprecvalues <- c()
+totprecvalues <- c()
+years <- c()
 
+for(x in 2011:2024){
+    years <- c(years, as.character(x))
+}
 
+for(x in 2011:2024){
+    var_name <- paste0("data_", x)
+    assign(var_name, mean(subset(nums2011_2024, format(DATE, "%Y") == as.character(x))$PRCP))
+    avgprecvalues <- c(avgprecvalues, mean(subset(nums2011_2024, format(DATE, "%Y") == as.character(x))$PRCP))
+}
+
+for(x in 2011:2024){
+    var_name <- paste0("total_", x)
+    assign(var_name, sum(subset(nums2011_2024, format(DATE, "%Y") == as.character(x))$PRCP))
+    totprecvalues <- c(totprecvalues, sum(subset(nums2011_2024, format(DATE, "%Y") == as.character(x))$PRCP))
+}
+
+print(years)
